b475cffd86c327fac97592efe95ad34ef69f905c
Naro <leejane311210@gmail.com>
2024-04-26 15:28:30 +0800

    Update chevrolet.js

--- chevrolet.js
+++ chevrolet.js
@@ -1,9 +1,10 @@
 /*
 雪佛兰app签到
-将cpctoken放到变量xflCookie里
+将抓包里的cpctoken放到xflCookie
 */
 
 const axios = require('axios')
+const notify = require('./sendNotify')
 
 axios.post(
   'https://ctwx.sgmlink.com/api/check/submit',
@@ -30,7 +31,7 @@ axios.post(
       'Cookie': 'SERVERCORSID=9471b821c927b7feb5455296fdefd3e0|1714113072|1714113071; SERVERID=9471b821c927b7feb5455296fdefd3e0|1714113072|1714113071; dcpDID=1714113072886_de7f5e34836c1; acw_tc=707c9fca17141130719525691e1598db722f88db6942339d6a7bbc6c462685'
     }
   }
-).then((res) => {console.log(res)})
+).then((res) => {console.log(res);notify.sendNotify('签到结果：',res.data)})
     .catch((error) => {
     console.error(error)
     })
